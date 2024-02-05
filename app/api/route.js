610ae1596a92bd26a87959ba2affb6b768eb0da2
Hardik29 <singhhardik14@gmail.com>
2024-02-05 18:25:12 +0530

    hgf

--- app/api/route.js
+++ app/api/route.js
@@ -4,7 +4,8 @@ async function getResponse(req) {//we are creating a post req
   console.log('API frame route called');
   return new NextResponse(`  <!DOCTYPE html><html><head> 
   <meta property="fc:frame" content="vNext" /> 
-  <meta property="fc:frame:image" content="https://i.pinimg.com/originals/15/b4/70/15b4702226b18c81cf3809f7f33d36b6.jpg" />   
+  <meta property="fc:frame:image" content="https://cyan-deep-moth-632.mypinata.cloud/ipfs/QmZUFzb9dYRFrYpXcp7MVNBThjWxyywEJAiF6jND8KkGrU/1%20(56).jpg" />   
+  <meta property="og:audio" content="https://cyan-deep-moth-632.mypinata.cloud/ipfs/QmeAU8s7mh2BaZM46hCNKHTxPhWHNpje6BiJuuSwpLQAwF" />
   </head></html> `);
 
 }
