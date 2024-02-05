adb34903f89bfd397d0352b0efe937ad2fd87fc5
Hardik29 <singhhardik14@gmail.com>
2024-02-05 17:17:41 +0530

    jk

--- app/api/route.js
+++ app/api/route.js
@@ -6,8 +6,6 @@ async function getResponse(req) {//we are creating a post req
   return new NextResponse(`  <!DOCTYPE html><html><head> 
   <meta property="fc:frame" content="vNext" /> 
   <meta property="fc:frame:image" content="https://cyan-deep-moth-632.mypinata.cloud/ipfs/QmZUFzb9dYRFrYpXcp7MVNBThjWxyywEJAiF6jND8KkGrU/1%20(${no}).jpg" />   
-  <meta property="fc:frame:button:1" content="Next Random Meme" />
-    <meta property="fc:frame:button:1:action" content="https://guileless-vacherin-9372b8.netlify.app/api" />
   </head></html> `);
 
 }
