e261bfdb8643b977115374c3f191307cf2c923eb
Hardik29 <singhhardik14@gmail.com>
2024-02-05 18:21:28 +0530

    hg

--- app/api/route.js
+++ app/api/route.js
@@ -6,7 +6,6 @@ async function getResponse(req) {//we are creating a post req
   return new NextResponse(`  <!DOCTYPE html><html><head> 
   <meta property="fc:frame" content="vNext" /> 
   <meta property="fc:frame:image" content="https://cyan-deep-moth-632.mypinata.cloud/ipfs/QmZUFzb9dYRFrYpXcp7MVNBThjWxyywEJAiF6jND8KkGrU/1%20(${no}).jpg" />   
-  <meta property="og:audio" content="https://cyan-deep-moth-632.mypinata.cloud/ipfs/QmeAU8s7mh2BaZM46hCNKHTxPhWHNpje6BiJuuSwpLQAwF" />
   </head></html> `);
 
 }
