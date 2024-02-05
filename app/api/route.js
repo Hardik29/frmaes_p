1922d8409ab5f9c037a2187e3cbc6501f037355a
Hardik29 <singhhardik14@gmail.com>
2024-02-05 18:37:51 +0530

    hgaa

--- app/api/route.js
+++ app/api/route.js
@@ -1,4 +1,4 @@
-import {  NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 async function getResponse(req) {//we are creating a post req
   console.log('API frame route called');
@@ -6,6 +6,12 @@ async function getResponse(req) {//we are creating a post req
   <meta property="fc:frame" content="vNext" /> 
   <meta property="fc:frame:image" content="https://cyan-deep-moth-632.mypinata.cloud/ipfs/QmZUFzb9dYRFrYpXcp7MVNBThjWxyywEJAiF6jND8KkGrU/1%20(56).jpg" />   
   <meta property="og:audio" content="https://cyan-deep-moth-632.mypinata.cloud/ipfs/QmeAU8s7mh2BaZM46hCNKHTxPhWHNpje6BiJuuSwpLQAwF" />
+  <body>
+  <audio controls>
+    <source src="https://cyan-deep-moth-632.mypinata.cloud/ipfs/QmeAU8s7mh2BaZM46hCNKHTxPhWHNpje6BiJuuSwpLQAwF" type="audio/mp3">
+    Your browser does not support the audio element.
+  </audio>
+</body>
   </head></html> `);
 
 }
