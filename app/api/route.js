914a3d5290054a55957a78c108c7d11fe5b993c7
Hardik29 <singhhardik14@gmail.com>
2024-02-05 15:43:54 +0530

    ha

--- app/api/route.js
+++ app/api/route.js
@@ -1,13 +1,11 @@
 import {  NextResponse } from 'next/server';
 
 async function getResponse(req) {//we are creating a post req
-  console.log('API frame route called');
+  
   return new NextResponse(`  <!DOCTYPE html><html><head> 
   <meta property="fc:frame" content="vNext" /> 
-  <meta property="fc:frame:image" content="https://drive.google.com/file/d/1R68Ujx3YK_LuP_1NLnfady0Q4_F64cC2/view?usp=sharing" /> 
-  <meta property="fc:frame:button:3" content="Try Again 😐😐" />
-  <meta property="fc:frame:button:3:action" content="https://enchanting-choux-aa3877.netlify.app/api" />
-  </head></html> `);
+  <meta property="fc:frame:image" content="https://imgs.xkcd.com/comics/biff_tannen.png" /> 
+  </head></html>`);
 
 }
 
--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
 
 const frameMetadata = getFrameMetadata({
-
   buttons: [
     {
       label: "Mint Nft" 
     },
   ],
-  image: 'https://drive.google.com/file/d/1R68Ujx3YK_LuP_1NLnfady0Q4_F64cC2/view?usp=sharing',
+  image: 'https://imgs.xkcd.com/comics/combination_vision_test.png',
 
   post_url: 'https://enchanting-choux-aa3877.netlify.app/api',
 });
@@ -18,7 +17,7 @@ export const metadata = {
   openGraph: { //open graph is a protocaol created by facebook, frames are built on top of these protocol
     title: 'Image',
     description: 'Cat Image',
-    images: ['Image'],
+    images: ['https://imgs.xkcd.com/comics/combination_vision_test.png'],
   },
   other: {
     ...frameMetadata,
